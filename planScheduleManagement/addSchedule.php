d63e76fcba114cc54b11a04d22a60eda6d643a83
Gammanpila <sithumigammanpila@gmail.com>
2025-02-26 11:19:21 +0530

    updated addSchedule.php

--- planScheduleManagement/addSchedule.php
+++ planScheduleManagement/addSchedule.php
@@ -28,18 +28,22 @@ $sDate = trim($data['sDate']);
 $eDate = trim($data['eDate']);
 $scheduleStatus = trim($data['scheduleStatus']);
 
-// Prepare SQL statement to insert employee data
-$stmt = $conn->prepare("INSERT INTO member_schedule (memberID, mpID, mplanName, sDate, eDate, scheduleStatus) VALUES (?, ?, ?, ?, ?, ?)");
-$stmt->bind_param("iissss", $memberID, $mpID, $mplanName, $sDate, $eDate, $scheduleStatus);
+try{
+    // Prepare SQL statement to insert schedule data
+    $stmt = $conn->prepare("INSERT INTO member_schedule (memberID, mpID, mplanName, sDate, eDate, scheduleStatus) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("iissss", $memberID, $mpID, $mplanName, $sDate, $eDate, $scheduleStatus);
 
-if ($stmt->execute()) {
-    ob_clean(); // Clear any unwanted output
-    echo json_encode(["success" => true, "message" => "Schedule added successfully!"]);
-} else {
+    if ($stmt->execute()) {
+        ob_clean(); // Clear any unwanted output
+        echo json_encode(["success" => true, "message" => "Schedule added successfully!"]);
+    } else {
+       throw new Exception("Failed to add schedule");
+    }
+} catch(Exception $e){
     http_response_code(500);
-    echo json_encode(["success" => false, "message" => "Failed to add schedule"]);
+    echo json_encode(["success" => false, "message" => $e->getMessage()]);
+} finally{
+    $stmt->close();
+    $conn->close();
+    exit;
 }
-
-$stmt->close();
-$conn->close();
-exit;
